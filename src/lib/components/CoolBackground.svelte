35b77f5f8f5b28abdb0cc01ed837fe243765f89f
fichince <acchoi@gmail.com>
2022-08-16 16:04:54 -0400

    Moving "big" layout to component

---
+++ src/lib/components/CoolBackground.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let animate : boolean = false;
+</script>
+
+<div class="absolute top-0 left-0 w-screen h-screen overflow-hidden">
+  <div class="homepage w-full h-full bg-cover scale-110 animate-blur-in blur-sm">
+  </div>
+  <div class="absolute 
+    top-1/4 left-1/4 
+    w-1/2 h-1/2 min-h-[450px]
+    m-auto 
+    bg-gradient-to-br from-primary to-secondary rounded-3xl"
+    class:animate-fade-in={animate}>
+
+    <slot></slot>
+  </div>
+</div>
+
+<style lang="postcss">
+  @import "@fontsource/special-elite";
+  @import "@fontsource/mononoki";
+
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  .homepage {
+    background-image: url(/images/wedge-pond.jpg);
+  }
+</style>
---
+++ src/routes/index@empty.svelte
@@ -0,0 +1,66 @@
+<script context="module" lang="ts">
+  // this page is entirely static with no JS interactivity needed
+  export const hydrate = false;
+  export const router = false;
+</script>
+
+<script>
+  import Icon from 'svelte-awesome';
+  import githubSquare from 'svelte-awesome/icons/githubSquare';
+  import facebookSquare from 'svelte-awesome/icons/facebookSquare';
+  import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
+  import CoolBackground from '$lib/components/CoolBackground.svelte';
+</script>
+
+
+<CoolBackground animate>
+  <div class="
+    h-32 w-32
+    sm:h-52 sm:w-52 
+    rounded-full overflow-hidden mx-auto 
+    mt-8 border-accent border-2">
+    <img src="/images/headshot.jpg" alt="headshot" />
+  </div>
+  <div class="mx-auto text-center mt-4 
+    text-2xl sm:text-3xl 
+    font-display text-accent">
+    Albert Choi
+  </div>
+  <div class="mx-auto text-center mt-1 
+    text-lg sm:text-xl
+    font-display text-accent">
+    I write code and sometimes prose.
+  </div>
+
+  <div class="flex flex-col sm:flex-row 
+    items-center
+    mx-auto w-2/3 sm:justify-evenly mt-4 
+    text-xl sm:text-2xl 
+    text-pop font-display">
+    <div class="hover:scale-110">
+      <a href="/blog">Blog</a>
+    </div>
+    <div class="hover:scale-110">
+      <a href="/prose">Prose</a>
+    </div>
+    <div class="hover:scale-110">
+      <a href="/">Code</a>
+    </div>
+  </div>
+
+  <div class="flex mt-4 w-2/3 mx-auto justify-evenly
+    text-pop text-2xl sm:text-4xl">
+    <a href="https://github.com/fichince" target="_blank" rel="noreferrer noopener"
+      class="hover:scale-110">
+      <Icon data={githubSquare} scale={3} />
+    </a>
+    <a href="https://www.facebook.com/albert.choi.583/" target="_blank" rel="noreferrer noopener"
+      class="hover:scale-110">
+      <Icon data={facebookSquare} scale={3} />
+    </a>
+    <a href="https://www.linkedin.com/in/albert-choi-202b8923/" target="_blank" rel="noreferrer noopener"
+      class="hover:scale-110">
+      <Icon data={linkedinSquare} scale={3} />
+    </a>
+  </div>
+</CoolBackground>
